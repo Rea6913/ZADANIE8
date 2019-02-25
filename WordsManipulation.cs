fde5f9a50fc185e5918952ce7c7747f3838c49db
Rea6913 <47655248+Rea6913@users.noreply.github.com>
2019-02-25 14:54:30 +0300

    8

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -7,8 +7,35 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+
+            if (text == null)
+                throw new ArgumentNullException();
+            if (text.Length == 0)
+                throw new ArgumentException();
+
+        string razd = ".,!? -:;";
+            string slova = "";
+            string otv = "";
+            string slovo_now = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (razd.Contains(text[i].ToString()))
+                {
+                    if (!slova.Contains(' ' + slovo_now + ' '))
+                    {
+                        slova += ' ' + slovo_now + ' ';
+                        otv += slovo_now + text[i];
+                    }
+                    else
+                        otv += text[i];
+                    slovo_now = "";
+                }
+                else
+                    slovo_now += text[i];
+            }
+            text = otv;
         }
     }
 }
+     
+
